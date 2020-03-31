b9689a538e04fcb4c34c6c1a314e04d243486559
Sam <melodyweaver1105@yahoo.com>
2020-03-31 00:29:44 -0400

    Create display table, tons of CSS styling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-    // src/App.js
+// src/App.js
 
-    import React, {Component} from 'react';
-    import Goal from "./components/goal";
+import React, { Component } from 'react';
+import Goal from "./components/goal";
+import Month from "./components/month";
 
-    class App extends Component {
+class App extends Component {
 
-      render () {
-        return (
-          <Goal/>
-        );
-      }
-    }
+  render() {
+    return (
+      <div>
+        <h1 text-allign="center">Getting Around To It</h1>
+        <Goal text-allign="center"/>
+        <br/>
+        <Month/>
+      </div>
 
-    export default App;
+    );
+  }
+}
+
+export default App;
--- src/components/goal.js
+++ src/components/goal.js
@@ -6,14 +6,32 @@ import "./goal.css"
 
 class Goal extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = { value: '' };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ value: event.target.value });
+    }
+
+    handleSubmit(event) {
+        alert('Goal has been submitted: ' + this.state.value);
+        event.preventDefault();
+    }
+
     render() {
         return (
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <div>
                     <textarea
-                        className="Goal"
-                        placeholder="One or two sentences that describe what you want to do">
-                    </textarea>
+                        placeholder="One or two sentences that describe what you want to do"
+                        value={this.state.value}
+                        onChange={this.handleChange}
+                    />
                 </div>
                 <div>
                     <input type="submit" />
